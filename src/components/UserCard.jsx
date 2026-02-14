442d2eea1a8201d70d071a5e5ebd237751f7b60d
Aditya Batchu <batchuaditya369@gmail.com>
2026-02-14 18:29:29 +0530

    send request from feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,52 @@
-const UserCard = ({user}) => {
-  const {photoURL,firstName,lastName, about, age, gender} = user;
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
+
+const UserCard = ({ user }) => {
+  const { photoURL, firstName, lastName, about, age, gender, _id } = user;
+  const dispatch = useDispatch();
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,{},{withCredentials:true}
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (error) {
+      throw new Error("ERROR: " + error.message);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center mx-10">
       <div className="card bg-base-100 w-96 h-120 shadow-sm">
-      <figure>
-        <img
-          src={photoURL}
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{firstName+"  "+lastName}</h2>
-        {age && gender && <p>{age}, {gender}</p>}
-        <p>
-          {about}
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
+        <figure>
+          <img src={photoURL} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{firstName + "  " + lastName}</h2>
+          {age && gender && (
+            <p>
+              {age}, {gender}
+            </p>
+          )}
+          <p>{about}</p>
+          <div className="card-actions justify-end">
+            <button
+              className="btn btn-primary"
+              onClick={() => handleSendRequest("ignored", _id)}
+            >
+              Ignore
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => handleSendRequest("interested", _id)}
+            >
+              Interested
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
